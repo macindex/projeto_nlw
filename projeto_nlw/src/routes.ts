85bb796bc87139cb6316332177af3d4ca089c928
marcio lira <machollanda@gmail.com>
2021-04-28 12:07:04 -0300

    MessagesController and service create

--- projeto_nlw/src/routes.ts
+++ projeto_nlw/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
@@ -7,6 +8,7 @@ const routes = Router();
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 /**
  * Tipos de parametros
@@ -22,5 +24,6 @@ const usersController = new UsersController();
 
 routes.post("/settings", settingsController.create);
 routes.post("/users", usersController.create);
+routes.post("/messages", messagesController.create);
 
 export { routes };
